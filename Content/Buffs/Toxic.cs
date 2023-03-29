55ee262b1b7a20ab4d9f2f876720d63c71cfe1cb
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-03-29 18:15:16 +0200

    Update Toxic.cs

--- Content/Buffs/Toxic.cs
+++ Content/Buffs/Toxic.cs
@@ -1,11 +1,4 @@
-using Terraria;
-using Terraria.ID;
-using Terraria.Modloader;
 
-namespace XelvaaMod.Content.debuff
-{
-public class Toxic : ModBuf
-{
 
 // le nouveau debuff doit faire 20 de dps (le poison pour info c'est 6) 
 // même effet visuel que le poison donc faut copier l'effet quoi
