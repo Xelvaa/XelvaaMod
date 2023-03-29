aafcab5aa3dcea3dba4ff7200a8f4206bf46e857
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-03-29 14:56:20 +0200

    Update PhantomPhoenix.cs

--- Global/Weapons/PhantomPhoenix.cs
+++ Global/Weapons/PhantomPhoenix.cs
@@ -1,1 +1,3 @@
-
+// regarder bien l'illustration de l'arme sur le wiki. Après le 3ème tir (le phoenix) y'a un espèce de mini cooldown
+//(en gros tu tirse pas pendant gerne 30 frames après le phoenix, alors que l'arme attaque de base tous les 18 frames),
+// je propose qu'on retire ce cooldown et le remettre à la normal, ça va améliorer le dps.
