c00cd76e6b0b77c4e2612106771a857b36715934
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-03-29 18:02:38 +0200

    Update FlareGun.cs

--- Global/Weapons/FlareGun.cs
+++ Global/Weapons/FlareGun.cs
@@ -1,5 +1,24 @@
 using Terraria;
 using Terraria.ID;
-using Terraria.Modloader;
+using Terraria.ModLoader;
 
-//Xelvaa - je m'en occupe 
+namespace XelvaaMod.Global.Weapons.FlareGun
+{
+
+	public class FlareGun : GlobalItem
+	{
+
+		public override bool AppliesToEntity(Item item, bool lateInstatiation) {
+			return item.type == ItemID.FlareGun;
+		}
+
+		public override void SetDefaults(Item item) {
+			item.StatsModifiedBy.Add(Mod); 
+			item.damage = 3;
+			item.DamageType = DamageClass.Ranged;
+			item.knockBack = 0f;
+		}
+	}
+
+//j'avais déjà testé ce truc auparavant ça marchait bien mais les reforges étaient quand même bloqués (vu que le flare gun de base ne peut pas avoir de reforge).
+//Une idée comment forcer l'arme à avoir des reforges ?
