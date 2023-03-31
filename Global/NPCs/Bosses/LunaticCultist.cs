13505201c37059279236f57cdcc8ac7d56e68232
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-03-31 09:56:22 +0200

    Update LunaticCultist.cs

--- Global/NPCs/Bosses/LunaticCultist.cs
+++ Global/NPCs/Bosses/LunaticCultist.cs
@@ -1,1 +1,2 @@
-
+//faudrait buff ses pv, actuellement il a que 32k/48K/61k et je pense que si il avait 40k/60k/80k, ça serait déjà beaucoup mieux (après si vous êtes pas d'accord
+// avec moi on peut en discuter
