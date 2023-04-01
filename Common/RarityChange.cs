3802f19845752ecc7838556ebe7adf13c3c7e429
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-01 18:01:39 +0200

    Update RarityChange.cs

--- Common/RarityChange.cs
+++ Common/RarityChange.cs
@@ -10,40 +10,45 @@ namespace Common.RarityChange
         {
             switch (item.type)
             {
-            case ItemID.AnkletoftheWind
-            case ItemID.FeralClaws
-            case ItemID.JungleHat
-            case ItemID.JungleShirt
-            case ItemID.JunglePants
-            case ItemID.IvyWhip
-            case ItemID.BladeofGrass
-            case ItemID.ThornChakram
-            case ItemID.JungleYoyo
-            case ItemID.ThornWhip
+                case ItemID.AnkletoftheWind:
+                case ItemID.FeralClaws:
+                case ItemID.JungleHat:
+                case ItemID.JungleShirt:
+                case ItemID.JunglePants:
+                case ItemID.IvyWhip:
+                case ItemID.BladeofGrass:
+                case ItemID.ThornChakram:
+                case ItemID.JungleYoyo:
+                case ItemID.ThornWhip:
             item.rare = ItemRarityID.Green;
-                    break;
-            case ItemID.SolarEruption
-            case ItemID.StardustCellStaff
-            case ItemID.VortexBeater
-            case ItemID.NebulaArcanum
-            case ItemID.StardustDragonStaff
-            case ItemID.DayBreak
-            case ItemID.NebulaBlaze
-            case ItemID.Phantasm
-            case ItemID.SuperHealingPotion
-            case ItemID.LunarCraftingStation
-            case ItemID.LunarHook
-            case ItemID.Terrarian
-            case ItemID.FairyQueenMagicItem
-            case ItemID.FairyQueenRangedItem
-            case ItemID.EmpressButterfly
-            case ItemID.PiercingStarlight
-            case ItemID.RainbowWhip
-            case ItemID.SparkleGuitar
-            item.rage = ItemRarityID.Cyan
             break;
-            case ItemID.EmpressBlade
-            case ItemID.Zenith
-            item.rage = ItemRarityID.Purple
+                case ItemID.SolarEruption:
+                case ItemID.StardustCellStaff:
+                case ItemID.VortexBeater:
+                case ItemID.NebulaArcanum:
+                case ItemID.StardustDragonStaff:
+                case ItemID.DayBreak:
+                case ItemID.NebulaBlaze:
+                case ItemID.Phantasm:
+                case ItemID.SuperHealingPotion:
+                case ItemID.LunarCraftingStation:
+                case ItemID.LunarHook:
+                case ItemID.Terrarian:
+                case ItemID.FairyQueenMagicItem:
+                case ItemID.FairyQueenRangedItem:
+                case ItemID.EmpressButterfly:
+                case ItemID.PiercingStarlight:
+                case ItemID.RainbowWhip:
+                case ItemID.SparkleGuitar:
+                    item.rare = ItemRarityID.Cyan;
+            break;
+                case ItemID.EmpressBlade:
+                case ItemID.Zenith:
+                    item.rare = ItemRarityID.Purple;
+            break;
+            }
+        }
+    }
+}
        
   
