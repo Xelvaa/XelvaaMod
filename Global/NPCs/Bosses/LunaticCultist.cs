2eaa9292eaac27bac7b16143c79fa06990fb5f5b
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-01 10:52:23 +0200

    Update LunaticCultist.cs

--- Global/NPCs/Bosses/LunaticCultist.cs
+++ Global/NPCs/Bosses/LunaticCultist.cs
@@ -1,2 +1,3 @@
 //faudrait buff ses pv, actuellement il a que 32k/48K/61k et je pense que si il avait 40k/60k/80k, ça serait déjà beaucoup mieux (après si vous êtes pas d'accord
-// avec moi on peut en discuter
+//avec moi on peut en discuter
+//Sinon j'aimerai bien qu'il drop son treasure bag inutilisé et qu'il puisse donner le terrarian, au lieu du moon lord
