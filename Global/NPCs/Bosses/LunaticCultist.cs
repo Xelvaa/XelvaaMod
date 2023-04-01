f7a020997d8fd5325f4e110b92aceaa56ace32c4
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-01 19:00:37 +0200

    Update LunaticCultist.cs

--- Global/NPCs/Bosses/LunaticCultist.cs
+++ Global/NPCs/Bosses/LunaticCultist.cs
@@ -1,3 +1,22 @@
-//faudrait buff ses pv, actuellement il a que 32k/48K/61k et je pense que si il avait 50k/75k/100k, ça serait déjà beaucoup mieux (après si vous êtes pas d'accord
-//avec moi on peut en discuter
-//Sinon j'aimerai bien qu'il drop son treasure bag inutilisé et qu'il puisse donner le terrarian, au lieu du moon lord
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Global.NPCs.Bosses
+{
+	public class LunaticCultist : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+        public override void SetDefaults(NPC npc)
+		{
+            if (npc.type == NPCID.CultistBoss)
+            {
+				npc.lifeMax = (int)(npc.lifeMax  * ((float)50000 / 25000));
+ 
