12456649888205d2aba9ff5b81fbef18a01a4877
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-01 11:09:43 +0200

    Update Terrarian.cs

--- Global/Weapons/Melee/Terrarian.cs
+++ Global/Weapons/Melee/Terrarian.cs
@@ -1,1 +1,21 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace XelvaaMod.Global.Weapons.Melee
+{
+
+	public class Terrarian : GlobalItem
+	{
+
+		public override bool AppliesToEntity(Item item, bool lateInstatiation) {
+			return item.type == ItemID.Terrarian;
+		}
+
+		public override void SetDefaults(Item item) {
+			item.StatsModifiedBy.Add(Mod); 
+			item.damage = 105;
+			item.knockBack = 4f;
+		}
+	}
+}
