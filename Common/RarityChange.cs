56cccff6b0e96a527a968197f040edefe0be367a
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-01 11:56:42 +0200

    Update RarityChange.cs

--- Common/RarityChange.cs
+++ Common/RarityChange.cs
@@ -1,1 +1,24 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace Common.RarityChange
+{
+    public class ChangesRarity : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            switch (item.type)
+            {
+            case ItemID.AnkletoftheWind
+            case ItemID.FeralClaws
+            case ItemID.JungleHat
+            case ItemID.JungleShirt
+            case ItemID.JunglePants
+            case ItemID.IvyWhip
+            case ItemID.BladeofGrass
+            case ItemID.ThornChakram
+            case ItemID.JungleYoyo
+            case ItemID.ThornWhip
+            item.rare = ItemRarityID.Green;
+                    break;
