e29468035c17be92c72af41ed76722e82a5ccabe
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-07 09:38:33 +0200

    Update GolemEye.cs

--- Global/Accessories/GolemEye.cs
+++ Global/Accessories/GolemEye.cs
@@ -1,1 +1,24 @@
-//Donne maintenant 15% de chances de coup critique, au lieu de 10%
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using XelvaaMod;
+using System.Collections.Generic;
+using Terraria.Utilities;
+using static Terraria.ModLoader.ModContent;
+
+namespace XelvaaMod.Global.Accessories
+{
+    public class Accessories : GlobalItem
+    {
+        
+        public override void UpdateAccessory(Item item, Player player, bool hideVisual)
+        {
+            switch (item.type)
+            {
+                            case ItemID.EyeoftheGolem:
+                    player.GetCritChance<GenericDamageClass>() += 14;
+                    break;
+                    }
+                    
