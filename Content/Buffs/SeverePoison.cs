0036e471d31d4d7d8732476af83031b988e47ba7
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-18 15:49:09 +0200

    Update SeverePoison.cs

--- Content/Buffs/SeverePoison.cs
+++ Content/Buffs/SeverePoison.cs
@@ -2,16 +2,23 @@ using Terraria;
 using Terraria.ID;
 using Terraria.Modloader;
 
-namespace XelvaaMod.Content.Buffs {
-    public class SeverePoison : ModBuff {
-        public override void SetStaticDefaults() {
+namespace XelvaaMod.Content.Buffs
+{
+    public class SeverePoison : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
             DisplayName.SetDefault("Severe Poison");
             Description.SetDefault("Slowly losing life");
             Main.debuff[Type] = true;
-            Main.pvpBuff[Type] = true;}
-        public override Update(Player player, ref int buffIndex){
+            Main.pvpBuff[Type] = true;
+            }
+        public override Update(Player player, ref int buffIndex)
+        {
             // le nouveau debuff doit faire 20 de dps (le poison pour info c'est 6) 
             // même effet visuel que le poison donc faut copier l'effet quoi
-            player.GetModPlayer<XelvaaModPlayer>().SeverePoison = true;}
+            player.GetModPlayer<XelvaaModPlayer>().SeverePoison = true;
+            }
+        
 }
 }
