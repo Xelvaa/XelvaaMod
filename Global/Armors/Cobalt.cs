b1d4fafe0131c1a6e780e31c988ee8c5d01c4f20
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-18 11:46:21 +0200

    Update Cobalt.cs

--- Global/Armors/Cobalt.cs
+++ Global/Armors/Cobalt.cs
@@ -1,1 +1,29 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Global.Armors
+{
+    public class Cobalt : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+        public override GlobalItem Clone(Item item, Item itemClone)
+        {
+            return base.Clone(item, itemClone);
+        }
+        public override void SetDefaults(Item item)
+        {
+            switch (item.type)
+            {
+             case ItemID.CobaltHat:
+             item.defense = 9;
+             return;
+             case ItemID.CobaltBreastplate:
+             item.defense = 12;
+             return;
+             case ItemID.CobaltLeggings:
+             item.defense = 10;
+             return;
+             }
+             }
 
