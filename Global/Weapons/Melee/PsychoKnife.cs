bbd52d4c370f89d5e62a47c147b20b05eaef6d37
Xelvaa <55628418+Xelvaa@users.noreply.github.com>
2023-04-20 11:39:42 +0200

    Update PsychoKnife.cs

--- Global/Weapons/Melee/PsychoKnife.cs
+++ Global/Weapons/Melee/PsychoKnife.cs
@@ -1,1 +1,20 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace Global.Weapons.Melee
+{
+	public class PsychoKnife : GlobalItem
+	{
+		public override bool AppliesToEntity(Item item, bool lateInstatiation) {
+			return item.type == ItemID.PsychoKnife;
+		}
+
+		public override void SetDefaults(Item item) {
+			item.StatsModifiedBy.Add(Mod);
+
+			item.damage = 50;
+      item.useStyle = ItemUseStyleID.Rapier;
+		}
